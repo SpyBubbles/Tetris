2d90aa0fb81ccc76ddb1eba312b6a3f6b8fc7a4e
SpyBubbles <160052026+SpyBubbles@users.noreply.github.com>
2024-05-11 10:23:05 +0000

    Version 6

--- src/tui_test.cpp
+++ src/tui_test.cpp
@@ -1,7 +1,8 @@
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
-#include <ftxui/dom/node.hpp>
-#include <ftxui/screen/color.hpp>
+#include <ftxui/dom/elements.hpp>   // for filler, text, hbox, vbox
+#include <ftxui/screen/screen.hpp>  // for Full, Screen
+#include <ftxui/dom/node.hpp>       // for Render
+#include <ftxui/screen/color.hpp>   // for ftxui
+#include <stdio.h>                  // for getchar
 #include <iostream>
 #include <string>
 #include <thread>
@@ -17,7 +18,9 @@ int main(int argc, char const *argv[])
     list<string> textos;
     fstream imagen;
     //imagen.open("./assets/Cubo/C.txt");
-    imagen.open("./assets/Linea/l1.txt");
+    imagen.open("./assets/Linea/l2.txt");
+    //imagen.open("./assets/Linea/l1.txt");
+    //imagen.open("./assets/L/L1.txt");
 
     string linea;
     while (getline(imagen,linea))
@@ -28,8 +31,11 @@ int main(int argc, char const *argv[])
 
     int fotograma = 0;
     string reset;
-    int posX = 0;
-    int posY = 6;
+    int AltoLimite = 20;
+    int AnchoLimite = 24;
+    int posX = 0; //18 Limite de altura
+    int posY = 1;    //limite Izquierda Cubo
+    //int posY = 20;  //limite Derecha depende de la pieza
 
     while (true){
     //fotograma++;
@@ -40,8 +46,8 @@ int main(int argc, char const *argv[])
         hbox() | colorFondo | colorTexto 
     });
 
-    Dimensions Alto = Dimension::Fixed(20);
-    Dimensions Ancho = Dimension::Fixed(25);
+    Dimensions Alto = Dimension::Fixed(AltoLimite);
+    Dimensions Ancho = Dimension::Fixed(AnchoLimite);
 
     Screen pantalla = Screen::Create(Ancho,Alto);
     Render (pantalla,dibujo);
@@ -58,12 +64,55 @@ int main(int argc, char const *argv[])
         l++;
     }
     posX++;
+    posY = posY + 2;
+
+    if (posX >= AltoLimite-2)
+    {
+        posX=0;
+    }
+
+    if (posY >= AnchoLimite-8)
+    {
+        posY=1;
+    }
 
     Render(pantalla,dibujo);
     pantalla.Print();
         reset = pantalla.ResetPosition();
     cout << reset;
-    this_thread::sleep_for(0.1s);
+    this_thread::sleep_for(0.6s);
     }
     return 0;
+    /*
+  auto cell = [](const char* t) { return text(t) | border; };
+  auto document =  //
+      gridbox({
+          {
+              
+              
+              cell("SCORE"),
+          },
+          {
+              //
+              gridbox({
+                  {
+                      //
+                      
+                  },
+                  {
+                      cell("LEVEL"),
+                      
+                  },
+              }),
+              //
+          },
+          {
+              cell("LINES"),
+          },
+      });
+  auto screen = Screen::Create(Dimension::Fit(document));
+  Render(screen, document);
+  screen.Print();
+  getchar();
+  */
 }
