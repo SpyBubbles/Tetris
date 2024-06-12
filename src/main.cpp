c9d5e07d67d671177c7fcb7a7a2dc08929f861f5
SpyBubbles <160052026+SpyBubbles@users.noreply.github.com>
2024-06-12 14:06:56 -0600

    Prueba final

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+/*#include <stdio.h>                  // for getchar
+#include <iostream>
+#include <string>
+#include <thread>
+#include <list>
+#include <fstream>
+#include <experimental/random>*/
 
 int main(int argc, char const *argv[])
 {
